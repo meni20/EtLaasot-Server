1fe54a67a3845b051dd0ee8b5b5a1f24e19de671
User <menilevi2005@gamil.com>
2026-01-21 20:57:56 +0200

    fix: correct import statement and enhance getAllUsers method to include UserRole

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -2,8 +2,8 @@ import { Transaction } from 'sequelize';
 import User from './entities/user.entity';
 import { Injectable } from '@nestjs/common';
 import { IUser } from './interfaces/user.interface';
-import UserRole from '../user-role/enitites/user-role.entity';
 import { AUTH_ROLES } from 'src/constants/auth.constants';
+import UserRole from '../user-role/enitites/user-role.entity';
 
 @Injectable()
 export default class UserRepository {
@@ -12,7 +12,14 @@ export default class UserRepository {
   }
 
   public async getAllUsers() {
-    return await User.findAll();
+    return await User.findAll({
+      include: [
+        {
+          model: UserRole,
+          attributes: ["roleId"],
+        },
+      ],
+    });
   }
 
   public async getAllVolunteers() {
